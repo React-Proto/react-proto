bfecd3b44be9fe66f251672aa588984f74cf5efe
briantaylor <brian.taylor818@gmail.com>
2018-07-27 10:52:59 -0700

    :sparkles: Add MaterialUI expansion panel to left column

--- src/containers/LeftContainer.jsx
+++ src/containers/LeftContainer.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/components';
+import LeftColExpansionPanel from '../components/LeftColExpansionPanel.jsx';
 
 const mapStateToProps = store => ({
-
+  components: store.components,
+  
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -22,16 +24,19 @@ class LeftContainer extends Component {
   }
 
   render() {
-    const { addComponent } = this.props;
+    console.log(this.props)
+    const { addComponent, components  } = this.props;
     const { inputValue } = this.state;
 
     return (
       <div className="column">
         <input type="text" placeholder="Add Component" onChange={event => this.handleChange(event)} />
         <button onClick={() => addComponent(inputValue)}>Add</button>
-      </div >
+        {components.map((component, i) => <LeftColExpansionPanel key={i} name={component.name} /> )}
+      </div>
     );
   }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(LeftContainer);
+
--- src/reducers/componentReducer.js
+++ src/reducers/componentReducer.js
@@ -1,6 +1,6 @@
 import * as types from '../actionTypes';
 
-const componentReducer = (state = [], action) => {
+const componentReducer = (state = [{ name: 'Erik' }], action) => {
   switch (action.type) {
     case types.ADD_COMPONENT:
       console.log(action.payload);
