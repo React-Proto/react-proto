91813fcc01dc30c595774ed0bf5348567e8a0948
briantaylor <brian.taylor818@gmail.com>
2018-08-11 15:30:53 -0700

    Added throttle to reduce function calls and improve app performance

--- src/store.js
+++ src/store.js
@@ -1,23 +1,23 @@
 import logger from 'redux-logger';
-import throttle from 'lodash/throttle';
+import throttle from 'lodash.throttle';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import { loadState, saveState } from './localStorage';
-
 
 const persistedState = loadState();
 
 const store = createStore(
   reducers,
-  composeWithDevTools(),
-  applyMiddleware(logger, thunk),
   persistedState,
+  compose(
+    applyMiddleware(logger, thunk),
+    composeWithDevTools(),
+  ),
 );
 
 store.subscribe(throttle(() => {
-  // debugger;
   return saveState(store.getState());
 }, 1000));
 
