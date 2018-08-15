b5df3cc230286a2586834e0c0c1e5d9901f6c5a8
briantaylor <brian.taylor818@gmail.com>
2018-08-15 16:03:27 -0700

    :sparkles: Added localForage for persisting state and offline data storage

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,21 +1,38 @@
-export const loadState = () => {
-  try {
-    const serializedState = localStorage.getItem('state');
-    if (serializedState === null) {
-      return undefined;
-    }
-    return JSON.parse(serializedState);
-  } catch (err) {
-    return undefined;
-  }
-};
+import localforage from 'localforage';
 
+// export const loadState = () => {
+//   try {
+//     const serializedState = localforage.getItem('state');
+//     if (serializedState === null) {
+//       return undefined;
+//     }
+//     return JSON.parse(serializedState);
+//   } catch (err) {
+//     return undefined;
+//   }
+// };
 
 export const saveState = (state) => {
-  try {
-    const serializedState = JSON.stringify(state);
-    localStorage.setItem('state', serializedState);
-  } catch (err) {
-    // Ignore write errors
-  }
+  localforage.setItem('state', state).then((value) => value)
+  .catch((err) => {
+    console.log(err);
+  });
 };
+
+export const loadState = () => localforage.getItem('state');
+
+// export const loadState = () => {
+//   localforage.setItem('state', function (err) {
+//     // if err is non-null, we got an error
+//   });
+// }
+
+// export const loadState = () => {
+//   const serializedState = localforage.getItem('state', function (err, value) {
+//     // if err is non-null, we got an error. otherwise, value is the value
+//     if(err) {
+//       return undefined;
+//     }
+//     return JSON.parse(serializedState);
+//   });
+// }
