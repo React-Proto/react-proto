c57177eb47852ce9026d95691bf5324413df046c
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2018-08-20 10:19:56 -0700

    Testing fix

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -73,7 +73,7 @@ module.exports = {
     },
   },
   plugins: [
-    new CleanWebpackPlugin([BUILD_DIR]),
+    // new CleanWebpackPlugin([BUILD_DIR]),
     new HtmlWebpackPlugin({
       template: 'public/index.html',
     }),
