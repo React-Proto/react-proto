e5a9f202da7b6a3b5f5bd5e4f2641aea01918151
Darryl Amour <darryl.amour@gmail.com>
2018-12-27 16:40:25 -0800

    Updated to JSONStream

--- src/utils/createWorkspaceFile.util.js
+++ src/utils/createWorkspaceFile.util.js
@@ -7,82 +7,86 @@
 import fs from 'fs';
 import path from 'path';
 import archiver from 'archiver';
+import JSONStream from 'JSONStream';
 
 const createWorkspaceFile = (workspaceData) => {
-  const replacer = (key, value) => {
-    // Filtering out properties
-    if (key === 'workspaceFilePath') return undefined;
-    if (key === 'imagePath') return path.basename(value);
-    return value;
-  };
-
   const newPromise = new Promise((resolve, reject) => {
-    // Create a meta file representing our state
-    // Serialize workspaceData Object to JSON
-    const serialize = JSON.stringify(workspaceData, replacer, 2);
-
     // Create name for JSON meta file
     const metaJSONFile = workspaceData.workspaceFilePath.replace(/.rproto$/i, '.json');
 
-    // Write serialized workspaceData to file
-    fs.writeFile(
-      metaJSONFile,
-      serialize,
-      { flag: 'w' },
-      (err) => {
-        if (err) reject(err);
-        // Zip the image & meta file together
-        // Create a file to stream archive data to
-        const rprotoOutput = fs.createWriteStream(workspaceData.workspaceFilePath);
-        const archive = archiver(
-          'zip',
-          {
-            comment: 'React-Proto Workspace archive',
-            zlib: { level: 9 }, // Maximum compression level
-          },
+    // Create a meta file representing our state
+    // Serialize workspaceData Object to JSON
+
+    // where to pipe to
+    const transformStream = JSONStream.stringifyObject('{', ',', '}');
+    const outputStream = fs.createWriteStream(metaJSONFile);
+    transformStream.pipe(outputStream);
+
+    // start writing to the pipe
+    const workspaceDataKeys = Object.keys(workspaceData);
+    for (let i = 0; i < workspaceDataKeys.length; i += 1) {
+      // Filtering out properties
+      // console.log(workspaceDataKeys[i]);
+      // console.log(workspaceData[workspaceDataKeys[i]]);
+      if (workspaceDataKeys[i] === 'imagePath') transformStream.write([workspaceDataKeys[i], path.basename(workspaceData[workspaceDataKeys[i]])]);
+      else if (workspaceDataKeys[i] !== 'workspaceFilePath') transformStream.write([workspaceDataKeys[i], workspaceData[workspaceDataKeys[i]]]);
+    }
+
+    // close the pipe
+    transformStream.end();
+
+    // Zip the image & meta file together
+    // Create a file to stream archive data to
+    outputStream.on('finish', () => {
+      const rprotoOutput = fs.createWriteStream(workspaceData.workspaceFilePath);
+      const archive = archiver(
+        'zip',
+        {
+          comment: 'React-Proto Workspace archive',
+          zlib: { level: 9 }, // Maximum compression level
+        },
+      );
+
+      // archiver has been finalized and the output file
+      // descriptor has closed.
+      rprotoOutput.on('close', () => {
+        // Clean-up by deleting .json file
+        fs.unlink(metaJSONFile, (unlinkErr) => {
+          if (unlinkErr) reject(unlinkErr);
+          resolve(workspaceData.workspaceFilePath);
+        });
+      });
+
+      // good practice to catch warnings
+      archive.on('warning', (archiveErr) => {
+        // throw error
+        reject(archiveErr);
+      });
+
+      // good practice to catch this error explicitly
+      archive.on('error', (archiveErr) => {
+        reject(archiveErr);
+      });
+
+      // pipe archive data to the file
+      archive.pipe(rprotoOutput);
+
+      // Append finalize the archive
+      // we are done appending files but streams have to finish yet
+      if (workspaceData.imagePath) {
+        archive.append(
+          fs.createReadStream(workspaceData.imagePath),
+          { name: path.basename(workspaceData.imagePath), comment: 'React-Proto image' },
         );
+      }
 
-        // archiver has been finalized and the output file
-        // descriptor has closed.
-        rprotoOutput.on('close', () => {
-          // Clean-up by deleting .json file
-          fs.unlink(metaJSONFile, (unlinkErr) => {
-            if (unlinkErr) reject(unlinkErr);
-            resolve(workspaceData.workspaceFilePath);
-          });
-        });
-
-        // good practice to catch warnings
-        archive.on('warning', (archiveErr) => {
-          // throw error
-          reject(archiveErr);
-        });
-
-        // good practice to catch this error explicitly
-        archive.on('error', (archiveErr) => {
-          reject(archiveErr);
-        });
-
-        // pipe archive data to the file
-        archive.pipe(rprotoOutput);
-
-        // Append finalize the archive
-        // we are done appending files but streams have to finish yet
-        if (workspaceData.imagePath) {
-          archive.append(
-            fs.createReadStream(workspaceData.imagePath),
-            { name: path.basename(workspaceData.imagePath), comment: 'React-Proto image' },
-          );
-        }
-
-        archive
-          .append(
-            fs.createReadStream(metaJSONFile),
-            { name: path.basename(metaJSONFile), comment: 'React-Proto metadata, JSON format' },
-          )
-          .finalize();
-      },
-    );
+      archive
+        .append(
+          fs.createReadStream(metaJSONFile),
+          { name: path.basename(metaJSONFile), comment: 'React-Proto metadata, JSON format' },
+        )
+        .finalize();
+    });
   });
   return newPromise;
 };
